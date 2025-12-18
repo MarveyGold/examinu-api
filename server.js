272254cc90d48bcb8ef6872f6293c9d2139078cd
Marvey Gold <145897274+MarveyGold@users.noreply.github.com>
2025-12-18 17:01:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ await app.register(cors, {
 const filePath = process.cwd() + `/data/data.json`;
 app.get('/api/data', async () => {
   const file = fs.readFileSync(filePath, 'utf8');
-  return JSON.parse(file)
+  const data = JSON.parse(file);
+  return data.map(f => f.name)
 });
 app.listen({ port: 8080, host: '0.0.0.0'}, () => {
   console.log("server started on port 8080")
